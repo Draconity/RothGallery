2b5bf27a36f49c172d9ba5ec8223d3d4a6e9a0c2
Selroth <selroth@thewestgate.net>
2016-04-07 14:33:11 -0700

    Added files via upload
    Adding centered zoom - incomplete.

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ var mouseInOut = false;
 var dragging = false;
 var dragStartMouseX = 0;
 var dragStartMouseY = 0;
+var zoomX = 0;
+var zoomY = 0;
 var anchorX = 0; //Anchor represents where on the picture (even if scaled) the user clicked.
 var anchorY = 0;
 var scaleFactor = 1; //Our "zoom" level.
@@ -16,7 +18,7 @@ var picY = 0;
 var debug = true; //Enables/disables certain features the conventional user won't care about.
 
 //Refreshes our mouse values
-function refreshMouseXY(e){
+function refreshMouseXYZ(e){
 	mouseX = Math.min(Math.max(e.pageX-mainCVS.offsetLeft, 0), mainCVS.width);
 	mouseY = Math.min(Math.max(e.pageY-mainCVS.offsetTop, 0), mainCVS.height);	
 	
@@ -27,8 +29,8 @@ function refreshMouseXY(e){
 
 //Refreshes the anchor point on the image.
 function refreshAnchorXY(){
-	anchorX = Math.round((mouseX - picX) / scaleFactor); 
-	anchorY = Math.round((mouseY - picY) / scaleFactor);	
+	anchorX = Math.round((mouseX/scaleFactor) - picX); 
+	anchorY = Math.round((mouseY/scaleFactor) - picY);	
 }
 
 //Redraws the canvas
@@ -37,12 +39,23 @@ function redraw(){
 	mainCTX.fillStyle = "rgba(0, 0, 0, 1);" //Set the fill style.
 	
 	//Draw the pic.
-	mainCTX.save();	//Zoom in/out
-	mainCTX.translate(picX, picY);
-	mainCTX.scale(scaleFactor, scaleFactor);			
-	mainCTX.fillRect(0, 0, 10, 10);
+	mainCTX.save();	
+	mainCTX.translate(zoomX, zoomY);
+	mainCTX.scale(scaleFactor, scaleFactor); //Zoom in/out			
+	mainCTX.translate(-zoomX, -zoomY);
+	mainCTX.fillRect(picX, picY, 10, 10); //Draw it
+	mainCTX.strokeStyle = "rgba(0,0,0,0.1)";
+	mainCTX.beginPath();
+	for(var x = 0; x < mainCVS.width; x = x + 10){
+		mainCTX.moveTo(x, 0);
+		mainCTX.lineTo(x, mainCVS.height);
+	}
+	for(var y = 0; y < mainCVS.height; y = y + 10){
+		mainCTX.moveTo(0, y);
+		mainCTX.lineTo(mainCVS.width, y);
+	}
+	mainCTX.stroke();
 	mainCTX.restore();
-	
 	
 	//Draw the text
 	if (debug) mainCTX.fillText(mouseX + " (" + anchorX + "), " + mouseY + " (" + anchorY + "), " + mouseZ, 0, 10);	
